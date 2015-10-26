688e2b9b245343c0c740143e2937008362d103d4
OdieD8 <odie_de_moura@hotmail.com>
2015-10-26 14:51:43 -0600

    First commit

---
+++ js/mainCtrl.js
@@ -0,0 +1,12 @@
+var app = angular.module("friendsList");
+
+app.controller("mainCtrl", function($scope) {
+    
+    $scope.name = "OdieD8"
+    $scope.friends = [
+        "Jack",
+        "Jill",
+        "Pail",
+        "Hill"
+        ]
+});
